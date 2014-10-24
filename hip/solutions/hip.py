509230443abe187797cdd90458db8954418abab2
Ryan Stringham <stringham@gmail.com>
2014-10-24 15:53:51 -0600

    fix python hip solution

--- hip/solutions/hip.py
+++ hip/solutions/hip.py
@@ -11,15 +11,16 @@ def getScores(board, size, players):
 			if grid[y1][x1] != '-':
 				for x2 in xrange(x1+1 if y1==size-1 else x1,size):
 					for y2 in xrange(0 if x2>x1 else y1+1,size):
-						dx = x1-x2
-						dy = y1-y2
-						x3 = x1+dy
-						x4 = x2+dy
-						y3 = y1-dx
-						y4 = y2-dx
-						if 0 <= x3 < size and 0 <= y3 < size and 0 <= x4 < size and 0 <= y4 < size \
-							and grid[y1][x1] == grid[y3][x3] == grid[y4][x4]:
-								scores[grid[y1][x1]] += 1
+						if grid[y1][x1] == grid[y2][x2]:
+							dx = x1-x2
+							dy = y1-y2
+							x3 = x1+dy
+							x4 = x2+dy
+							y3 = y1-dx
+							y4 = y2-dx
+							if 0 <= x3 < size and 0 <= y3 < size and 0 <= x4 < size and 0 <= y4 < size \
+								and grid[y1][x1] == grid[y3][x3] == grid[y4][x4]:
+									scores[grid[y1][x1]] += 1
 	printScores(scores)
 
 for _ in xrange(input()):
